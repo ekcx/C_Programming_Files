c1848b1d2e93deb926d6fcb146329f92522ddb0e
Kocatas <kocatasefe@gmail.com>
2022-05-23 09:26:10 +0300

    Adding Files

---
+++ Numerical_Analysis/RungeKutta_Method.c
@@ -0,0 +1,17 @@
+//
+// Created by Kocatas on 5/23/2022.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void runge_kutta(double, double, double (*)(double, double), double, double);
+
+
+int main() {
+
+
+    double ta, tb, tc, td;
+
+}
+
