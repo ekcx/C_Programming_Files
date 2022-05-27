dced1152c40b803691712a20751b00d780b68a81
Kocatas <kocatasefe@gmail.com>
2022-05-27 20:12:52 +0300

    Adding Files

--- SortingAlgorithms/Line_Graphic.c
+++ SortingAlgorithms/Line_Graphic.c
@@ -12,6 +12,10 @@ int main(){
 
     int i,j;
     double m,n;
+    // önceden verilmiş olan koordinatlarla işlem yapıyoruz.
+    // (i1,j1) ve  (i2,j2)
+    // Doğru denklemi => y = m*x + c
+
     double j1 = 5.0;
     double j2 = 70.0;
 
@@ -19,11 +23,11 @@ int main(){
 
         for(j = 0; j < 80; j++) {
             m = 50.0 / 65.0;
-            n = fabs(i - (m * (j - 5)) + 10);
+            n = fabs(i - (m * (j - 5)) + 10); // koordinatlara göre olan eğim denklemi.
 
             if (n < 0.8) {
                 if (min(j1, j2) && max(j1, j2)) {
-                    printf("*");
+                    printf("*"); // // Eğim denklemi üzerinde yer alan noktada * bastırılması.
                 }
             } else {
                 printf("o");
